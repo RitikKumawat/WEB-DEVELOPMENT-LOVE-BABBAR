5f6e7d769731f1aac18eaa79c509e4ef2c729392
RitikKumawat <rtkkmwt@gmail.com>
2023-08-26 16:32:23 +0530

    react Learning

--- LearnReact/demoshopapp/src/App.js
+++ LearnReact/demoshopapp/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Item from './components/item';
+import ItemDate from './components/ItemDate';
+import Card from './components/Card';
 function App() {
+  const response=[
+    {
+      itemName:"Nirma",
+      itemDate:"20",
+      itemYear:"1992"
+    },
+    {
+      itemName:"Surf Excel",
+      itemDate:"22",
+      itemYear:"1892"
+    },
+    {
+      itemName:"555",
+      itemDate:"19",
+      itemMonth:"July",
+      itemYear:"1890"
+    }
+  ];
+ 
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+  <div>
+    <Card>
+    
+      <Item name="Nirma" ></Item>
+      <ItemDate day="20" month="June" year="1998"></ItemDate>
+
+      <Item name="SurfExcel"></Item>
+      <ItemDate day="22" month="December" year="1990"></ItemDate>
+
+      <Item name={response[2].itemName}></Item>
+      <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
+      
+      <div classname="App">Hello Jee</div>
+    
+    </Card>
+      
+      
+  </div>
+
+
   );
 }
 
---
+++ LearnReact/demoshopapp/src/components/item.js
@@ -0,0 +1,8 @@
+import './item.css'
+
+function Item(props){
+    const itemName=props.name
+    
+    return (<p className="nirma">{itemName}</p>);
+}
+export default Item;
