b864e07ae4e112f42a014f8915d02510b461c099
RitikKumawat <rtkkmwt@gmail.com>
2023-08-31 17:35:45 +0530

    learn React

--- LearnReact/demoshopapp/src/components/ProductItem.js
+++ LearnReact/demoshopapp/src/components/ProductItem.js
@@ -5,14 +5,20 @@ import Card from './Card';
 import './ProductItem.css';
 
 const ProuductItem = (props) =>{
+    
+    function clickHandler(){
+        console.log("Button Clicked");
+    }
+    
+    
     return (
         <Card className="product-item">
             <ProductDate date={props.date} />
             <div className="product-item_description">
                 <h2>{props.title}</h2>
-
-
             </div>
+            <button onClick={clickHandler}>Add To Cart</button>
+
         </Card>
     )
 }
--- LearnReact/demoshopapp/src/components/Products.js
+++ LearnReact/demoshopapp/src/components/Products.js
@@ -1,7 +1,6 @@
 import  React from 'react';
 import ProductItem from './ProductItem';
 import Card from './Card';
-
 import './Products.css';
 
 const Products = (props) =>{
@@ -10,22 +9,22 @@ const Products = (props) =>{
             <ProductItem 
                 title={props.items[0].title}
                 amount={props.items[0].amount}
-                date= {props.item[0].date}
+                date= {props.items[0].date}
             />
             <ProductItem 
                 title={props.items[1].title}
                 amount={props.items[1].amount}
-                date= {props.item[1].date}
+                date= {props.items[1].date}
             />
             <ProductItem 
                 title={props.items[2].title}
                 amount={props.items[2].amount}
-                date= {props.item[2].date}
+                date= {props.items[2].date}
             />
             <ProductItem 
                 title={props.items[3].title}
                 amount={props.items[3].amount}
-                date= {props.item[3].date}
+                date= {props.items[3].date}
             />
         </Card>
     );
