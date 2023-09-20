d7102fee4ef35c574bdce466a96c7b3c7bafecb4
RitikKumawat <rtkkmwt@gmail.com>
2023-09-20 15:54:33 +0530

    Mini project Blog website

---
+++ Blog React Website/blogwebsite/src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Spinner.css" 
+function Spinner() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      
+      <div className='spinner'></div>
+
+    </div>
+  )
+}
+
+export default Spinner
