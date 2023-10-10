b84cce9fd9d9c77e2f5b77c3ddc7331714680902
RitikKumawat <rtkkmwt@gmail.com>
2023-10-10 16:26:15 +0530

    new files

--- Auth App/controllers/Auth.js
+++ Auth App/controllers/Auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 //Signup Route
 exports.signup = async (req,res)=>{
     try {
@@ -43,4 +44,67 @@ exports.signup = async (req,res)=>{
             message:"User cannot registered, please try again later",
         })
     }
-} 
+}
+
+exports.login = async(req,res)=>{
+    try {
+        //data fetch
+        const {email,password} = req.body;
+        //validation on email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the details carefully",
+            });
+        }
+        //check for registered user
+        let user = await User.findOne({email});
+        //if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered",
+            });
+        }
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        };
+        //Verify password & generate a JWT token
+        if(await bcrypt.compare(password,user.password)){
+            //password matches
+            let token = jwt.sign(payload,process.env.JWT_SECRET,
+                                                        {
+                                                        expiresIn:"2h",
+                                                        }
+            );
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+            const options ={
+                expires: new Date( Date.now()+ 3*24*60*60*1000),
+                httpOnly:true,
+            }
+            res.cookie("token",token,options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:"User Logged in successfully",
+            });
+        }
+        //password do not match
+        else{
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect",
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Login Failure",
+        })
+    }
+}
--- Auth App/routes/user.js
+++ Auth App/routes/user.js
@@ -2,9 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 const{login, signup} = require("../controllers/Auth");
+const{auth,isStudent,isAdmin} = require("../middleware/auth");
 
-
-// router.post("/login",login);
+router.post("/login",login);
 router.post("/signup",signup);
+
+
+//Testing middleware 
+router.get("/test",auth,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome to the protected route for Tests",
+    });
+})
+//Protected Route == only authorized can see this routes 
+//such as admin can see his/her admin dashboard
+//and student can see their student dashboard
+router.get("/student",auth,isStudent,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome to the protected route for Students",
+    });
+})
+
+router.get("/admin",auth,isAdmin,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome to the protected route for Admin",
+    });
+})
+
 
 module.exports = router;
